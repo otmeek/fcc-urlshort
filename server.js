85d1c04dc9f6114a9e52d78dd50d183f7d4d84d4
Mafalda Pereira <otmeek@gmail.com>
2016-02-28 20:57:39 +0000

    Finish basejump

--- server.js
+++ server.js
@@ -1,31 +1,136 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+var path = require('path');
+var mongo = require('mongodb').MongoClient;
+var validUrl = require('valid-url');
 
 var app = express();
 require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+app.get('/:URL', function(req, res) {
+	var url = req.params.URL;
+	var obj;
+	
+	mongo.connect('mongodb://localhost:27017/urls', function(err, db) {
+		if(err) throw err;
+		
+		var values = db.collection('values');
+		
+		values.find({
+			"short_url": 'https://fcc-urlshort-otmeek.c9users.io/' + url 
+		}, {
+			"original_url": 1
+		}).toArray(function(err, docs) {
+			if(err) throw err;
+			if(docs.length > 0) {
+				var dest = docs[0]["original_url"];
+				res.writeHead(301, {
+					Location: dest
+				});
+				res.end();
+				db.close();
+			}
+			else {
+				obj = {
+					"error": "No short url found for given input"
+				}
+				res.send(obj);
+				db.close();
+			}
+		});
+	});
+		
+});
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
+app.get('/new*', function(req, res) {
+	var originalUrl = req.path.substring("/new/".length);
+	var newUrl = '';
+	var obj;
+		
+	mongo.connect('mongodb://localhost:27017/urls', function(err, db) {
+		if (err) throw err;
+		
+		var counter = db.collection('counter');
+		var values = db.collection('values');
+		
+		// generate url
+		function getNewUrl() {
+			var randomNo = Math.round(Math.random() * (9999 - 0) + 0);
+			newUrl = 'https://fcc-urlshort-otmeek.c9users.io/' + randomNo.toString();
+			values.find({
+				"short_url": newUrl
+			}, {
+				"original_url": 1,
+				"short_url": 1,
+				_id: 0
+			}).toArray(function(err, docs) {
+				if (err) throw err;
+				if(docs.length > 0)
+					getNewUrl();
+			});
+			return newUrl;
+		}
+		
+		// first, check if url has already been shortened
+		values.find({
+			"original_url": originalUrl
+		}, {
+			"original_url": 1,
+			"short_url": 1,
+			_id: 0
+		}).toArray(function (err, docs) {
+			if(err) throw err;
+			if(docs.length > 0) {
+				obj = docs;
+				console.log(obj);
+				res.send(obj[0]);
+				db.close();
+			}
+			else {
+				if(validUrl.isUri(originalUrl)) {
+					// write to db
+					values.insert({
+						"original_url": originalUrl,
+						"short_url": getNewUrl()
+					}, function(err) {
+						if (err) throw err;
+						values.find({
+							"original_url": originalUrl
+						}, {
+							"original_url": 1,
+							"short_url": 1,
+							_id: 0
+						}).toArray(function(err, docs) {
+							if (err) throw err;
+							res.send(docs[0]);
+							db.close();
+						})
+					});
+				}
+				else {
+					// url is not valid
+					obj = {
+						"error": "invalid URL"
+					}
+					res.send(obj);
+					db.close();
+				}
+			}
+		})
+		
+	});
 
-app.use(passport.initialize());
-app.use(passport.session());
+});
 
-routes(app, passport);
+app.get('/:URLSTRING', function(req, res) {
+	var urlStr = req.params.URLSTRING;
+	
+	// check if urlStr is in database
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
